f5fa1263e14d2ebb6d80d619638471728bd585b1
Elias <zambiasi@LAPTOP-MPKJ75OB.localdomain>
2020-06-16 12:18:41 +0200

    Fixed A

--- problems/Minimization/A/main.cs
+++ problems/Minimization/A/main.cs
@@ -9,10 +9,12 @@ public class main{
         {
             return (1-x[0])*(1-x[0])+ 100*(x[1]-x[0]*x[0])*(x[1]-x[0]*x[0]);
         };
+        WriteLine("The Rosenbruck Valley function: f(x,y)=(1-x)^2+100(y-x^2)^2");
         vector start = new vector(new double []{1.3,0.2});
+        start.print("starting point              : ");
         var min_rosen = minimization.qnewton(f_rosen,ref start,epsilon);
         vector x_rosen = new vector(new double[]{1.0,1.0});
-        WriteLine("The Rosenbruck Valley function: f(x,y)=(1-x)^2+100(y-x^2)^2");
+        
         start.print("Quasi newton minimum        : ");
         x_rosen.print($"The exact minimum           : ");
         WriteLine($"Number of steps             : {min_rosen} steps.");
@@ -21,13 +23,14 @@ public class main{
 
         Func<vector,double> f_himmel = (x) =>
         {
-            return (x[0]*x[0]+x[1]-11)*(x[0]*x[0]+x[1]-11) + (x[0]-x[1]*x[1]-7)*(x[0]-x[1]*x[1]-7);
+            return (x[0]*x[0]+x[1]-11)*(x[0]*x[0]+x[1]-11) + (x[0]+x[1]*x[1]-7)*(x[0]+x[1]*x[1]-7);
         };
         epsilon = 1e-4;
-        vector start_h = new vector(new double []{-1,-1});
+        WriteLine("The Himmelblau function: f(x,y)=(x^2+y-11)^2+(x+y^2-7)^2.");
+        vector start_h = new vector(new double []{1,1});
+        start_h.print("starting point              : ");
         vector x_himmel = new vector(new double []{3.0,2.0});
         var min_himmel = minimization.qnewton(f_himmel,ref start_h,epsilon);
-        WriteLine("The Himmelblau function: f(x,y)=(x^2+y-11)^2+(x+y^2-7)^2.");
         start_h.print("Quasi newton minimum        : ");
         x_himmel.print($"The exact minimum           : ");
         WriteLine($"Number of steps             : {min_himmel} steps.");
