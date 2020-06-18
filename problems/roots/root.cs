19ccd81d2d8074951a6eb7d369bc9d650834a718
Elias <zambiasi@LAPTOP-MPKJ75OB.localdomain>
2020-06-18 11:11:30 +0200

    Did roots A

--- problems/roots/root.cs
+++ problems/roots/root.cs
@@ -4,28 +4,27 @@ public static class root{
 
     public static vector newton(
 	                    Func<vector,vector> f, //takes the input vector x and retrun f(x)
-	                    vector x, //Starting point
-	                    double eps, //The accuracy goal, ||f(x)||< epsilon
-	                    double dx=1e-7 //finite difference
+	                    vector xstart, //Starting point
+	                    double eps=1e-3, //The accuracy goal, ||f(x)||< epsilon
+	                    double dx=1e-6 //finite difference
                         ) 
-    {   
-	vector root = x.copy();
-    int n = root.size;
+    {    
+	vector x = xstart.copy();
+    int n = x.size;
     matrix J = new matrix(n,n);
-    
 	do
     {
-        var fx = f(root);
+        var fx = f(x);
         for(int j = 0; j<n;j++)
         {
-            root[j] += dx;
-            var df = f(root)-fx;
+            x[j] += dx;
+            var df = f(x)-fx;
             for(int i = 0; i<n;i++)
             {
                 J[i,j] = df[i]/dx;
                 
             }
-            root[j] -= dx;
+            x[j] -= dx;
         }  
         var QR = new qrdecompositionGS(J);
         var J_QR = QR.Q*QR.R;
@@ -36,22 +35,22 @@ public static class root{
         do
         {
             s/=2;
-            y = root + Dx*s;
+            y = x + Dx*s;
             fy = f(y);
-            if(fy.norm()<(1-s/2)*fx.norm() || s < 1.0/32)
+            if(fy.norm()<(1-s/2)*fx.norm() || s < 0.02)
             {
                 break;
             }
         }
         while(true);
-        root = x+s*Dx;
-        fx = f(root);
-        if(fx.norm()<eps)
+        x = y;
+        fy = f(y);
+        if(Dx.norm()<dx || fx.norm()<eps)
         {
             break;
         }
     }
     while(true);
-	return root;
+	return x;
     }
 }
